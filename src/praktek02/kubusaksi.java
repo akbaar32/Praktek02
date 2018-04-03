5c62a8548589c127324dc7f783c620311d4111b1
akbaar32 <Muhammad.akbaar32@gmail.com>
2018-04-03 11:46:19 +0700

    mengisi nilai atribut

--- src/praktek02/kubusaksi.java
+++ src/praktek02/kubusaksi.java
@@ -3,6 +3,9 @@ package praktek02;
 public class kubusaksi {
     public static void main(String[] args) {
         kubus k1 = new kubus();
+        k1.rusuka=13;
+        k1.rusukb=17;
+        k1.rusukc=14;
     }
     
 }
