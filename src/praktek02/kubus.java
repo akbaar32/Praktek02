85ed0e00ecca4c7dff24f0ea45d7114a0a7bd488
akbaar32 <Muhammad.akbaar32@gmail.com>
2018-04-03 11:39:39 +0700

    membuat atribut dan behavior

--- src/praktek02/kubus.java
+++ src/praktek02/kubus.java
@@ -1,4 +1,15 @@
 package praktek02;
 public class kubus {
+    double rusuka;
+    double rusukb;
+    double rusukc;
+    
+    void cetakinfo(){
+        System.out.println("=========================");
+        System.out.println("Rusuk A = "+rusuka);
+        System.out.println("Rusuk B = "+rusukb);
+        System.out.println("Rusuk C = "+rusukc);
+        System.out.println("=========================");
+    }
     
 }
